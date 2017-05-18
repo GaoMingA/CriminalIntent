3d88523e0f72b4613e3baa431f2d55a226c3dfcb
gaoming <gao_ming_1990@163.com>
2017-05-19 00:47:10 +0800

    增加RecyclerView 布局

--- app/src/main/java/com/gaoming/example/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/gaoming/example/criminalintent/CrimeListActivity.java
@@ -2,10 +2,13 @@ package com.gaoming.example.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by gaoming on 2017/5/17.
+ */
 
+public class CrimeListActivity extends SingleFragmentActivity{
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
