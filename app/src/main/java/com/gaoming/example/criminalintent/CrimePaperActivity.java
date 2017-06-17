94a6a9a8a19f561ea0af875df6add3d43ce1a4de
gaoming <gao_ming_1990@163.com>
2017-06-17 18:54:48 +0800

    添加工具栏

--- app/src/main/java/com/gaoming/example/criminalintent/CrimePaperActivity.java
+++ app/src/main/java/com/gaoming/example/criminalintent/CrimePaperActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,7 +17,7 @@ import java.util.UUID;
  * Created by gaoming on 2017/6/16.
  */
 
-public class CrimePaperActivity extends FragmentActivity {
+public class CrimePaperActivity extends AppCompatActivity {
 
     private static final String EXTRA_CRIME_ID =
             "com.bignerdranch.android.criminalintent.crime_id";
--- app/src/main/java/com/gaoming/example/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/gaoming/example/criminalintent/SingleFragmentActivity.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by gaoming on 2017/5/17.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity{
+public abstract class SingleFragmentActivity extends AppCompatActivity{
 
     protected abstract Fragment createFragment();
 
